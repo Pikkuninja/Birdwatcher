9d537be0933b948a7867ae8da329d8a7862e6c0c
Pikkuninja <jsaarinen92@gmail.com>
2019-05-09 21:05:18 +0300

    Remove direct dependency to ViewModelFactory from fragment  by injecting it as a ViewModelProvider.Factory instead

--- app/src/main/java/fi/jara/birdwatcher/screens/observationdetails/ObservationDetailsFragment.kt
+++ app/src/main/java/fi/jara/birdwatcher/screens/observationdetails/ObservationDetailsFragment.kt
@@ -1,17 +1,17 @@
 package fi.jara.birdwatcher.screens.observationdetails
 
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import fi.jara.birdwatcher.common.di.observationdetails.ObservationDetailsModule
 import fi.jara.birdwatcher.screens.common.BaseFragment
-import fi.jara.birdwatcher.screens.common.ViewModelFactory
 import javax.inject.Inject
 
 class ObservationDetailsFragment: BaseFragment() {
     private lateinit var viewModel: ObservationDetailsViewModel
 
     @Inject
-    lateinit var viewModelFactory: ViewModelFactory
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
