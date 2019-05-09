a7533f7454e3d5875b956222d2c7a42f05199e4b
Pikkuninja <jsaarinen92@gmail.com>
2019-05-09 21:02:00 +0300

    Fix Kotlin ViewModelKey annotation by bumping Kotlin version, use IntoMap with ObservationDetails VM as well

--- app/src/main/java/fi/jara/birdwatcher/screens/observationdetails/ObservationDetailsFragment.kt
+++ app/src/main/java/fi/jara/birdwatcher/screens/observationdetails/ObservationDetailsFragment.kt
@@ -1,13 +1,17 @@
 package fi.jara.birdwatcher.screens.observationdetails
 
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import fi.jara.birdwatcher.common.di.observationdetails.ObservationDetailsModule
 import fi.jara.birdwatcher.screens.common.BaseFragment
+import fi.jara.birdwatcher.screens.common.ViewModelFactory
 import javax.inject.Inject
 
 class ObservationDetailsFragment: BaseFragment() {
+    private lateinit var viewModel: ObservationDetailsViewModel
+
     @Inject
-    lateinit var viewModelFactory: ObservationDetailsViewModelFactory
+    lateinit var viewModelFactory: ViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +21,11 @@ class ObservationDetailsFragment: BaseFragment() {
             .observationDetailsModule(ObservationDetailsModule(this))
             .build()
             .inject(this)
+
+        subscribeToViewModel()
+    }
+
+    fun subscribeToViewModel() {
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(ObservationDetailsViewModel::class.java)
     }
 }
