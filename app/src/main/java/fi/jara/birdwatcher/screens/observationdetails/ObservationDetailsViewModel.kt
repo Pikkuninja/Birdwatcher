2b2b74e9bd97657abb1f3197e93386254cdbb302
Pikkuninja <jsaarinen92@gmail.com>
2019-05-11 21:04:13 +0300

    Add communication between viewmodel and fragment to observation details screen

--- app/src/main/java/fi/jara/birdwatcher/screens/observationdetails/ObservationDetailsViewModel.kt
+++ app/src/main/java/fi/jara/birdwatcher/screens/observationdetails/ObservationDetailsViewModel.kt
@@ -1,7 +1,12 @@
 package fi.jara.birdwatcher.screens.observationdetails
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+import fi.jara.birdwatcher.common.LiveEvent
+import fi.jara.birdwatcher.common.LoadingInitial
+import fi.jara.birdwatcher.common.NotFound
+import fi.jara.birdwatcher.common.ValueFound
+import fi.jara.birdwatcher.data.StatusSuccess
+import fi.jara.birdwatcher.observations.Observation
 import fi.jara.birdwatcher.observations.ObserveSingleObservationsUseCase
 
 class ObservationDetailsViewModel(
@@ -9,16 +14,41 @@ class ObservationDetailsViewModel(
     private val observationId: Long
 ) : ViewModel() {
 
-}
+    val observation: LiveData<Observation>
 
-class ObservationDetailsViewModelFactory(
-    private val useCase: ObserveSingleObservationsUseCase,
-    private val observationId: Long
-) :
-    ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        @Suppress("UNCHECKED_CAST")
-        return ObservationDetailsViewModel(useCase, observationId) as T?
-            ?: throw IllegalArgumentException("ObservationDetailsViewModelFactory can't create ViewModels of class ${modelClass.canonicalName}")
+    private val _showLoading = MutableLiveData<Boolean>()
+    val showLoading: LiveData<Boolean>
+        get() = _showLoading
+
+
+    private val _observationLoadErrors = MutableLiveData<String?>().apply { value = null }
+    val observationLoadErrors: LiveData<String?>
+        get() = _observationLoadErrors
+
+    init {
+        observation = MediatorLiveData<Observation>().apply {
+            addSource(observeSingleObservationsUseCase.execute(observationId)) { resultOrError ->
+                if (resultOrError.result != null) {
+                    when (val result = resultOrError.result) {
+                        is ValueFound -> {
+                            postValue(result.value)
+                            _observationLoadErrors.postValue(null)
+                            _showLoading.postValue(false)
+                        }
+                        is LoadingInitial -> {
+                            _observationLoadErrors.postValue(null)
+                            _showLoading.postValue(true)
+                        }
+                        is NotFound -> {
+                            _observationLoadErrors.postValue("Observation not found")
+                            _showLoading.postValue(false)
+                        }
+                    }
+                } else {
+                    _observationLoadErrors.postValue(resultOrError.errorMessage)
+                    _showLoading.postValue(false)
+                }
+            }
+        }
     }
 }
