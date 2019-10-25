dfe98128613b6a13974c3aa2ffaa3eecc775817f
Pikkuninja <jsaarinen92@gmail.com>
2019-10-25 17:14:46 +0300

    Update repositories with Kotlin Flows, convert to LiveData in UseCases

--- app/src/test/java/fi/jara/birdwatcher/mocks/MockObservationRepository.kt
+++ app/src/test/java/fi/jara/birdwatcher/mocks/MockObservationRepository.kt
@@ -1,23 +1,23 @@
 package fi.jara.birdwatcher.mocks
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import fi.jara.birdwatcher.data.*
 import fi.jara.birdwatcher.observations.Observation
 import fi.jara.birdwatcher.observations.ObservationSorting
-import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.consumeAsFlow
 
 // Google doesn't provide a way to test Room databases on non-device tests
 // https://developer.android.com/training/data-storage/room/testing-db
 
 class MockObservationRepository: ObservationRepository {
-    val allObservationsLiveData = MutableLiveData<RepositoryLoadingStatus<List<Observation>>>()
-    val singleObservationLiveData = MutableLiveData<RepositoryLoadingStatus<Observation>>()
+    val allObservationsChannel = Channel<RepositoryLoadingStatus<List<Observation>>>()
+    val singleObservationChannel = Channel<RepositoryLoadingStatus<Observation>>()
     var addObservationReturnValue: RepositoryLoadingStatus<Observation>? = null
 
-    override fun allObservations(sorting: ObservationSorting): LiveData<RepositoryLoadingStatus<List<Observation>>> = allObservationsLiveData
+    override fun allObservations(sorting: ObservationSorting): Flow<RepositoryLoadingStatus<List<Observation>>> = allObservationsChannel.consumeAsFlow()
 
-    override fun singleObservation(id: Long): LiveData<RepositoryLoadingStatus<Observation>> = singleObservationLiveData
+    override fun singleObservation(id: Long): Flow<RepositoryLoadingStatus<Observation>> = singleObservationChannel.consumeAsFlow()
 
     override suspend fun addObservation(observationData: NewObservationData) = addObservationReturnValue ?: throw IllegalStateException("Set return value for addObservation before calling it")
 }
