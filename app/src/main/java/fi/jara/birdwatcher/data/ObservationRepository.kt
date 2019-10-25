eeb8bd697a401b3c61ac533387344b012296abcd
Pikkuninja <jsaarinen92@gmail.com>
2019-10-25 18:21:13 +0300

    Remove comment about using Flow in Repository

--- app/src/main/java/fi/jara/birdwatcher/data/ObservationRepository.kt
+++ app/src/main/java/fi/jara/birdwatcher/data/ObservationRepository.kt
@@ -7,17 +7,7 @@ import fi.jara.birdwatcher.observations.ObservationSorting
 import kotlinx.coroutines.flow.Flow
 import java.util.*
 
-/*
-    Using LiveData in repository layer adds dependency to Android libraries and
-    that's kind of bad when RxJava could be used to achieve the same thing and has
-    more options on operators and threading. RxJava isn't included in the project
-    as the intention is to handle async with coroutines especially when Flows
-    come out of early access, and its easier to fix this one thing than pull out RxJava
-    that has been used to full extent
- */
-
 interface ObservationRepository {
-    // TODO: Use Kotlin Flows when out of early access
     fun allObservations(sorting: ObservationSorting): Flow<RepositoryLoadingStatus<List<Observation>>>
 
     fun singleObservation(id: Long): Flow<RepositoryLoadingStatus<Observation>>
